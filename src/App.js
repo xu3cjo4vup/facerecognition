49282855251b16b0a23172ecf5bc18946a05e570
xu3cjo4vup <lee_huihsin@hotmail.com>
2020-09-30 22:26:39 +0800

    add state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <div className='logo'>
-        <Logo />
-        <Rank />
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      fileLink: '',
+      NumberOfFaces : 0,
+    }
+  }
+
+  onChangeLink(event) {
+    console.log(event.target.value)
+  }
+
+  onClickDetect(event) {
+    console.log('event')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <div className='logo'>
+          <Logo />
+          <Rank />
+        </div>
+        
+        <ImageLinkForm loadLink={this.onChangeLink} clickDetect={this.onClickDetect}/>
+        {/*<FaceRecognition />*/}
       </div>
-      
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
-    </div>
-  );
+    )
+  }
+  
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({loadLink,clickDetect}) => {
+
   return (
     <div>
     	<p className='f3'>{'This magic brain will detect faces in your pictures. Give it a try.'}</p>
        <div className='form center pa4 br3 shadow-5'>
-				<input className='f4' type='text' style={{width:'70%'}}></input>	
-				<button className='f4 pa2'style={{width:'30%'}}>Detect</button>
+				<input className='f4' type='text' style={{width:'70%'}} 
+              onChange={loadLink} 
+        ></input>	
+				<button className='f4 pa2'style={{width:'30%'}}
+              onClick={clickDetect}
+        >Detect</button>
        </div> 
     </div>
   )
