fb8f349b8315f4f2d04079ec36ea2692edae6165
xu3cjo4vup <lee_huihsin@hotmail.com>
2020-10-03 23:26:14 +0800

    add loadimage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, {Component} from 'react';
+import Clarifai from 'clarifai'
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js'
 import './App.css';
 
+const app = new Clarifai.App({
+  
+  
+  apiKey: "YOUR API KEY",
+});
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      fileLink: '',
-      NumberOfFaces : 0,
+      input: '',
+      imageUrl: '',
+     
     }
   }
 
-  onChangeLink(event) {
-    console.log(event.target.value)
+  onChangeLink = (event) => {
+    
+    this.setState({input: event.target.value } )
   }
 
-  onClickDetect(event) {
-    console.log('event')
-  }
-
+  onClickDetect = (event)=> {
+    
+    this.setState({imageUrl: this.state.input } )
+    
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        // THE JPG
+        "https://clarifai.com/cms-assets/20180320221617/face-004.jpg"  
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+ 
   render() {
     return (
       <div className="App">
@@ -33,7 +56,7 @@ class App extends Component {
         </div>
         
         <ImageLinkForm loadLink={this.onChangeLink} clickDetect={this.onClickDetect}/>
-        {/*<FaceRecognition />*/}
+        <FaceRecognition />
       </div>
     )
   }
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FaceRecognition=() => {
+  return(
+		<div>
+			<img src={'https://clarifai.com/cms-assets/20180320221615/face-001.jpg'} alt='face' style={{width:'500px'}}/>
+		</div>  
+  )
+}
+
+export default FaceRecognition
